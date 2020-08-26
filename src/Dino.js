470daefbd0a00caffb552e7276c687d887a0e1b0
yoel <yoel@macbook-pro.home>
2020-08-26 15:42:55 -0700

    [dino-blog] add form edits and notes

--- src/Dino.js
+++ src/Dino.js
@@ -40,3 +40,42 @@ class Dino extends Component {
   }
 
 export default Dino;
+
+// WITH FORM EDITS Notes
+
+// constructor(props) {
+//   super()
+//   this.state = {
+//     body: props.body
+//     body: props.body,
+//     tempBody: ''
+//   }
+//   // this.handleBodyEdit = this.handleBodyEdit.bind(this)
+// }
+// @@ -21,6 +22,11 @@ class Dino extends Component {
+//   this.setState({body: input})
+// }
+
+// handleFormEdit = e => {
+//   e.preventDefault()
+//   this.setState({ body: this.state.tempBody})
+// }
+
+// render(){
+//   let allComments = this.props.comments.map((c, i)=>{
+//     return <Comment key={i} body={c}/>
+// @@ -30,7 +36,13 @@ class Dino extends Component {
+//       <h1>Check out my {this.props.title} blog!</h1>
+//       <p>Written by {this.props.author}</p>
+//       <p>{this.state.body}</p>
+//       <button onClick={this.handleBodyEdit}>Edit the body of this article</button>
+//       <hr />
+//       <form onSubmit={this.handleFormEdit}>
+//         <h5>New Body content</h5>
+//         <input type="text" name="body" onChange={e => this.setState({ tempBody: e.target.value })} />
+//         <input type="submit" />
+//       </form>
+//       {/* <button onClick={this.handleBodyEdit}>Edit the body of this article</button> */}
+//       {allComments}
+//     </>
+//   )
